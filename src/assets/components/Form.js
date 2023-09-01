ac7dc40df289fedf2d7170082b226859a823c818
Kin Ishii <kinishii97@gmail.com>
2023-09-02 01:36:01 +0900

    splitting components and adding handler for events

---
+++ src/assets/components/Form.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const Form = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+  
+  const changeHandler = (input, value) => {
+    console.log(input, value);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className="form">
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            type="number"
+            id="current-savings"
+            onChange={(event) => {
+              changeHandler("current-savings", event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            type="number"
+            id="yearly-contribution"
+            onChange={(event) => {
+              changeHandler("yearly-contribution", event.target.value);
+            }}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            type="number"
+            id="expected-return"
+            onChange={(event) => {
+              changeHandler("expected-return", event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            type="number"
+            id="duration"
+            onChange={(event) => {
+              changeHandler("duration", event.target.value);
+            }}
+          />
+        </p>
+      </div>
+      <p className="actions">
+        <button type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
+};
+export default Form;
